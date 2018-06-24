6f8ebe030eaa5c27ccfe63203b6eabf8e18dc8f4
HAMISH LYALL <101614208@student.swin.edu.au>
2018-06-24 11:25:01 +1000

    Insertion Sort Complete.

--- Dip_SWD_SubTask10_Insertion-ShellSorts/Program.cs
+++ Dip_SWD_SubTask10_Insertion-ShellSorts/Program.cs
@@ -9,24 +9,52 @@ namespace Dip_SWD_SubTask10_Insertion_ShellSorts
 {
     class Program
     {
+        private static object strings;
+
         static void Main(string[] args)
         {
             using (var reader = new StreamReader(@"D:\Users\Hamish\source\repos\Dip_SWD_SubTask10_Insertion-ShellSorts\unsorted_numbers.csv"))
             {
+                //Reads every element in file and adds it to a list.
                 List<string> listA = new List<string>();
                 while(!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split(';');
+                    var values = line.Split(' ');
                     listA.Add(values[0]);
                 }
-
-                foreach (var element in listA)
-                    Console.WriteLine(element);
-
+                //Converts string list into int list
+                List<int> listB = listA.Select(int.Parse).ToList();
+                //Then converts int list into int array (If there's a better way to do this I'd like to know about it.)
+                int[] arrayB = listB.ToArray();
+                //Sorts array using method below.
+                performInsertionSort(arrayB);
+                foreach (var element in arrayB)
+                Console.WriteLine(element);
                 Console.WriteLine("Done");
-                Console.ReadKey();
+                Console.ReadKey(); 
             }
+
+        }
+        static int[] performInsertionSort(int[] inputarray)
+        {
+            for (int i = 0; i < inputarray.Length - 1; i++)
+            {
+                int j = i + 1;
+
+                while (j > 0)
+                {
+                    if (inputarray[j - 1] > inputarray[j])
+                    {
+                        int temp = inputarray[j - 1];
+                        inputarray[j - 1] = inputarray[j];
+                        inputarray[j] = temp;
+
+                    }
+                    j--;
+                }
+            }
+            return inputarray;
         }
     }
 }
